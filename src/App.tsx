1cb99d93b86724079ec10893fc9fc82c6f3af6a3
fufudev <akmelf20@gmail.com>
2025-04-15 22:43:38 +0300

    feat(dashboard): init dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@apollo/client"
 import { gql } from "./__generated__";
 import { LoginForm } from "./components/login-form";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 const GET_USERS = gql(`
   query GetUsers {
@@ -30,10 +32,6 @@ function DisplayUsers() {
 export default function App() {
   
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
-      </div>
-    </div>
+    <Dashboard />
   )
 }
