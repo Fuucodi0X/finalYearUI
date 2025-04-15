ebae18a79b59633f90cd9e8e79265f8d79559a5b
fufudev <akmelf20@gmail.com>
2025-04-15 23:10:57 +0300

    fix: remove "use client"

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,5 @@
-import { useQuery } from "@apollo/client"
-import { gql } from "./__generated__";
-import { LoginForm } from "./components/login-form";
-import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 
-const GET_USERS = gql(`
-  query GetUsers {
-    users {
-      id
-      name
-      email
-    }
-  }
-`)
-
-function DisplayUsers() {
-  const { loading, error, data } = useQuery(GET_USERS);
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-
-  return data && data.users?.map(({id, name, email }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <b>Email:</b>
-      <p>{email}</p>
-    </div>
-  ))
-}
 
 export default function App() {
   
--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { MailIcon, PlusCircleIcon, type LucideIcon } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
